5e451b45ceccb719c01ca841d8a87eeb3547ad0b
Ajay <ajay11537@gmail.com>
2018-07-05 10:01:49 +0530

    Tool classification
    It is used to train images and and save trained model

--- tool_classification.py
+++ tool_classification.py
@@ -1,10 +1,9 @@
-# set the matplotlib backend so figures can be saved in the background
-import matplotlib
+
 matplotlib.use("Agg")
 from tqdm import tqdm
 # import the necessary packages
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam,SGD
+from keras.optimizers import Adam, SGD
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import img_to_array
 from keras.utils import to_categorical
@@ -24,16 +23,15 @@ import argparse
 import random
 import cv2
 import os
-TF_CPP_MIN_LOG_LEVEL=2
+
+TF_CPP_MIN_LOG_LEVEL = 2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset")
+                help="path to input dataset")
 ap.add_argument("-m", "--model", required=True,
-	help="path to output model")
-ap.add_argument("-p", "--plot", type=str, default="plot.png",
-	help="path to output accuracy/loss plot")
+                help="path to output model")
 args = vars(ap.parse_args())
 
 # initialize the number of epochs to train for, initial learning rate,
@@ -42,6 +40,7 @@ EPOCHS = 10
 INIT_LR = 0.001
 BS = 32
 
+
 # initialize the data and labels
 print("[INFO] loading images...")
 data = []
@@ -49,17 +48,16 @@ labels = []
 
 # grab the image paths and randomly shuffle them
 imagePaths = sorted(list(paths.list_images(args["dataset"])))
+# imagePaths = sorted(list(paths.list_images("/home/ajay/Downloads/All_61326/train_61326/new_all_images")))
 random.seed(42)
 random.shuffle(imagePaths)
 
 # loop over the input images
-i=0
+i = 0
 for imagePath in tqdm(imagePaths):
     # load the image, pre-process it, and store it in the data list
     # print imagePath
     image = cv2.imread(imagePath)
-    # image = cv2.resize(image, (256, 256))
-
     image = img_to_array(image)
     data.append(image)
     # extract the class label from the image path and update the
@@ -77,30 +75,26 @@ for imagePath in tqdm(imagePaths):
         label = 4
     elif "front" in label:
         label = 5
-    i=i+1
-    if i>10000:
-        break
     labels.append(label)
-print len(data)
-print len(labels)
+print(len(data))
+print(len(labels))
 # scale the raw pixel intensities to the range [0, 1]
 data = np.array(data, dtype="float")
 labels = np.array(labels)
 
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
-(trainX, testX, trainY, testY) = train_test_split(data,labels, test_size=0.25, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
 
 # convert the labels from integers to vectors
 trainY = to_categorical(trainY, num_classes=6)
 testY = to_categorical(testY, num_classes=6)
-print trainY[0]
+print(trainY[0])
 
 # construct the image generator for data augmentation
 aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
-	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-	horizontal_flip=True, fill_mode="nearest")
-
+                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+                         horizontal_flip=True, fill_mode="nearest")
 
 
 def build(width, height, depth, classes):
@@ -148,7 +142,7 @@ def build(width, height, depth, classes):
     model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, 3, 3, activation='relu', name='conv5_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-        
+
     # first set of CONV => RELU => POOL layers
     # model.add(Conv2D(20, (5, 5), padding="same",activation="relu"))
     # model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
@@ -157,11 +151,11 @@ def build(width, height, depth, classes):
     # model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     # first (and only) set of FC => RELU layers
     model.add(Flatten())
-    model.add(Dense(256,activation="relu"))
+    model.add(Dense(256, activation="relu"))
     model.add(Dropout(0.5))
 
     # softmax classifier
-    model.add(Dense(classes,activation="softmax"))
+    model.add(Dense(classes, activation="softmax"))
 
     # return the constructed network architecture
     return model
@@ -169,19 +163,15 @@ def build(width, height, depth, classes):
 
 # initialize the model
 print("[INFO] compiling model...")
-model = build(width=256, height=256, depth=3, classes=6)
-print model.summary()
+model = build(width=128, height=128, depth=3, classes=6)
+print(model.summary())
 
 opt = SGD(lr=INIT_LR, decay=1e-6, momentum=0.5, nesterov=True)
-model.compile(loss="binary_crossentropy", optimizer=opt,metrics=["accuracy"])
+model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # train the network
 print("[INFO] training network...")
-H = model.fit(trainX,trainY, batch_size=BS,epochs=EPOCHS, verbose=1)
-# H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
-# 	validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
-# 	epochs=EPOCHS, verbose=1)
-
+H = model.fit(trainX, trainY, batch_size=BS, epochs=EPOCHS, verbose=1)
 # save the model to disk
 print("[INFO] serializing network...")
 model.save(args["model"])
